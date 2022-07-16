303f03fb8042ea035f6ba2c8cc2dfda2b01c2617
Andrei <ilikeorange121@gmail.com>
2022-07-16 20:23:54 +0300

    almost finished compiler

--- src/main.rs
+++ src/main.rs
@@ -6,40 +6,46 @@ use debug_print::debug_print;
 use colored::Colorize;
 use std::fs::File;
 use std::io::prelude::*;
+use pulldown_cmark::{html, Parser};
 
 fn main() {
 
     // open file
-    let mut file = File::open("poem.txt")
+    let mut file = File::open("example.txt")
         .expect("File not found")
     ;
 
-
     // creating the data variable
-    let mut data = String::new().replace("\r\n", "<br>\n");
+    let mut data = String::new();
 
     // we read from "file" to the variable data.
     file.read_to_string(&mut data)
         .expect("Error while reading file")
     ;
 
-    //debug_print!("{}",&data);
-    data.to_string();
+    // crlf conversion
+    let data_lf = data.replace("\r\n", "<br>\n");
+    // split all the lines and put them inside a vector
+    let data_srt_vector: Vec<&str> = data_lf.split("\n").collect();
 
-    let data_array: Vec<&str> = data.split("\n").collect();
-
-    // printing all characters of the array
-    for i in 0..data_array.len() {
-        println!("{}", data_array[i]);
+    // debug - printing all lines
+    for i in 0..data_srt_vector.len() {
+        println!("{}", data_srt_vector[i]);
     }
 
-    for line in data.lines() {
-        print!("line nr?\n");
-    }
+    // debuging
+    println!("Number of lines: {}", data_srt_vector.len());
+    println!("Number of \"!\" : {}", data_srt_vector.concat().matches("!").count());
 
-    //println!("{}{}",format!("\nAfter Replace\n").red(), data.replace("\r\n", "<br>\n"));
 
-    // do this with all the string form array
-    // let P = format!("<p>{}</p>", data_array[i]);
+    // testing
+
+    let parser = Parser::new(data_lf.as_str());
+    let mut html_buf = String::new();
+    html::push_html(&mut html_buf, parser);
+
+    std::fs::write("write_test.txt", data_lf).expect("Unable to write file");
+
+    print!("{}", &html_buf);
 
 }
